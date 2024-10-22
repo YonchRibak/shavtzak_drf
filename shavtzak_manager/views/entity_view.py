8cb63479249288cb731d1f65937f55c176150bf6
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-10-22 17:31:25 +0300

    created entity_view.py

---
+++ shavtzak_manager/views/entity_view.py
@@ -0,0 +1,31 @@
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from ..models import Entity, UserType
+from django.core.exceptions import PermissionDenied
+
+class EntityView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        # Check if the user has the appropriate type to create an entity
+        if not request.user.user_system_custom_fields.user_type == UserType.ENTITY_INITIALIZER.value:
+            raise PermissionDenied("Only entity initializers can create entities.")
+
+        name = request.data.get('name')
+
+        # Create the entity with the logged-in superuser as the creator
+        entity = Entity.objects.create(
+            name=name,
+            creator=request.user
+        )
+
+        # Return a JSON response with the created entity data
+        return Response({
+            'id': entity.id,
+            'name': entity.name,
+            'code': entity.code,
+            'creator': entity.creator.email,
+            'created_at': entity.created_at.isoformat(),
+        }, status=status.HTTP_201_CREATED)

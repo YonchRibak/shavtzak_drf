2d10386fd1baf97016bc0ae50e1ecb54eff5953b
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2024-10-23 00:38:29 +0300

    updated entity view and add user view

--- shavtzak_manager/views/add_user_view.py
+++ shavtzak_manager/views/add_user_view.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from ..serializers import AddUserSerializer
+from django.contrib.auth.models import Group
 
 # View to add a user to an existing entity
 class AddUserView(APIView):
@@ -13,11 +14,12 @@ class AddUserView(APIView):
         
         if serializer.is_valid():
             user = serializer.save()
+          
             return Response({
                 'message': 'User added to the entity successfully.',
                 'user_id': user.id,
                 'email': user.email,
-                'entity': user.group.name
+                'groups': list(user.groups.values_list('name', flat=True))
             }, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

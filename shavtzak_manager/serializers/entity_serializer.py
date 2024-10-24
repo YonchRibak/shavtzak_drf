ae38f6c51657c82d90916720e9838ab1c077a727
omer bental <ombental@gmail.com>
2024-10-24 09:34:05 +0300

    show drf field

--- shavtzak_manager/serializers/entity_serializer.py
+++ shavtzak_manager/serializers/entity_serializer.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
 from ..models import Entity
 
 
 class EntitySerializer(serializers.ModelSerializer):
+    name = serializers.CharField(validators=[UniqueValidator(queryset=Entity.objects.all(), message='Name must be unique')])
     class Meta:
         model = Entity
         fields = ['id', 'name', 'description', 'created_at', 'code', 'creator', 'group']
